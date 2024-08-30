19b69c74ce9b61a27c1ab1bc1257e81ea1f8fe0c
nouhtech <161375331+nouhtech@users.noreply.github.com>
2024-08-30 12:06:36 +0100

    Create twst.sh

---
+++ twst.sh
@@ -0,0 +1,2 @@
+    { MODKEY,                       XK_f,          spawn,                  SHCMD("$HOME/.config/rofi/rofi-wifi-menu.sh") }, // run rofi-wifi-menu.sh
+    { MODKEY,                       XK_u,          spawn,                  SHCMD("$HOME/.config/rofi/powermenu.sh") }, // run powermenu.sh
